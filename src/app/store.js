034a218966d9ea436f528315fe73372d521de495
Taufiqurrahman Idrus <taufiqmck@gmail.com>
2022-10-11 09:15:00 +0800

    ready to deploy

--- src/app/store.js
+++ src/app/store.js
@@ -10,7 +10,8 @@ export const store = configureStore({
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(apiSlice.middleware),
-  devTools: true
+  devTools: false
 })
+// devTools: false
 
 setupListeners(store.dispatch)
